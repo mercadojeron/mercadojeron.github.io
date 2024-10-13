d4ee85853d8c0b499720e9f8e35a7fe303c86a65
Jeron Mercado <kimberjer18@gmail.com>
2024-10-13 20:52:24 +0800

    Replace old files with new ones

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+<<<<<<< HEAD
     console.log('Document loaded');
 });
+=======
+    alert("Welcome to my personal website!");
+  });
+  
+>>>>>>> b9f754b (Initial commit)
